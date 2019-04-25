baf456887196381842bb970ffa13ebc7c23c899b
Hoang <hoangq@firestak.com>
2019-04-25 16:14:36 +0700

    + Init project

---
+++ HPGradientLoading/HPGradientLoading.h
@@ -0,0 +1,19 @@
+//
+//  HPGradientLoading.h
+//  HPGradientLoading
+//
+//  Created by Hoang on 4/25/19.
+//  Copyright © 2019 Quang Hoang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HPGradientLoading.
+FOUNDATION_EXPORT double HPGradientLoadingVersionNumber;
+
+//! Project version string for HPGradientLoading.
+FOUNDATION_EXPORT const unsigned char HPGradientLoadingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HPGradientLoading/PublicHeader.h>
+
+
---
+++ HPGradientLoadingExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HPGradientLoadingExample
+//
+//  Created by Hoang on 4/25/19.
+//  Copyright © 2019 Quang Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
